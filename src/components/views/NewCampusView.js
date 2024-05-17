738b5aa1a1b62bd42caf09f0eeb66f5c986b2537
hayimheron <102389144+hayimheron@users.noreply.github.com>
2024-05-17 03:10:19 -0400

    Update NewCampusView.js

--- src/components/views/NewCampusView.js
+++ src/components/views/NewCampusView.js
@@ -6,7 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles( () => ({
     formContainer:{  
       width: '500px',
-      backgroundColor: '#f0f0f5',
+      backgroundColor: 'white',
       borderRadius: '5px',
       margin: 'auto',
     },
@@ -16,11 +16,11 @@ const useStyles = makeStyles( () => ({
       textDecoration: 'none'
     }, 
     customizeAppBar:{
-      backgroundColor: '#11153e',
+      backgroundColor: 'white',
       shadows: ['none'],
     },
     formTitle:{
-      backgroundColor:'#c5c8d6',
+      backgroundColor:'white',
       marginBottom: '15px',
       textAlign: 'center',
       borderRadius: '5px 5px 0px 0px',
@@ -40,27 +40,27 @@ const useStyles = makeStyles( () => ({
         <div className={classes.root}>
           <div className={classes.formContainer}>
             <div className={classes.formTitle}>
-              <Typography style={{fontWeight: 'bold', fontFamily: 'Courier, sans-serif', fontSize: '20px', color: '#11153e'}}>
-                Add a Campus
+              <Typography style={{fontWeight: 'bold', fontFamily: 'Courier, sans-serif', fontSize: '20px', color: 'black'}}>
+                Add Campus
               </Typography>
             </div>
             <form style={{textAlign: 'center'}} onSubmit={(e) => handleSubmit(e)}>
-                <label style= {{color:'#11153e', fontWeight: 'bold'}}>Name: </label>
+                <label style= {{color:'black', fontWeight: 'bold'}}>Name: </label>
                 <input type="text" name="name" required onChange ={(e) => handleChange(e)} />
                 <br/>
                 <br/>
   
-                <label style={{ color: '#11153e', fontWeight: 'bold' }}>Image URL: </label>
+                <label style={{ color: 'black', fontWeight: 'bold' }}>Image URL: </label>
                 <input type="text" name="imageurl" onChange={(e) => handleChange(e)} />
                 <br/>
                 <br/>
 
-                <label style={{color:'#11153e', fontWeight: 'bold'}}>Address: </label>
+                <label style={{color:'black', fontWeight: 'bold'}}>Address: </label>
                 <input type="text" name="address" required onChange={(e) => handleChange(e)} />
                 <br/>
                 <br/>
     
-                <label style={{ color: '#11153e', fontWeight: 'bold' }}>Description: </label>
+                <label style={{ color: 'black', fontWeight: 'bold' }}>Description: </label>
                 <textarea name="description" onChange={(e) => handleChange(e)} rows={4} cols={50}/>
                 <br/>
                 <br/>
